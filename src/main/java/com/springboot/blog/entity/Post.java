92e8d07e7a39860a4618f5f2d76c75d644733d55
gustavo.gavancho_dev <gustavo.gavancho@devsu.com>
2023-11-27 21:04:40 -0500

    Init

---
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -0,0 +1,30 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
