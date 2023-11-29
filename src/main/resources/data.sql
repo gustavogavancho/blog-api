e85f4b0b977cfd78855098876650e95b65b0c1c8
gustavo.gavancho_dev <gustavo.gavancho@devsu.com>
2023-11-29 15:34:49 -0500

    Post controller tests

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO posts (title, description, content) VALUES ('Post 1', 'Post 1 Description', 'Post 1 Content')
+INSERT INTO posts (title, description, content) VALUES ('Post 2', 'Post 2 Description', 'Post 2 Content')
+INSERT INTO posts (title, description, content) VALUES ('Post 3', 'Post 3 Description', 'Post 3 Content')
--- src/test/java/com/springboot/blog/controller/PostControllerTests.java
+++ src/test/java/com/springboot/blog/controller/PostControllerTests.java
@@ -1,4 +1,132 @@
 package com.springboot.blog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.service.PostService;
+import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PostController.class)
 public class PostControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+
+    private EasyRandom generator;
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+
+        generator = new EasyRandom();
+        mapper = new ObjectMapper();
+    }
+
+    @Test
+    public void getPostsSuccessfully() throws Exception {
+
+        // Arrange
+        var mockPosts = generator.objects(PostDto.class, 3).collect(Collectors.toList());
+        when(postService.getAllPosts()).thenReturn(mockPosts);
+        var postDtoListJson = mapper.writeValueAsString(mockPosts);
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(postDtoListJson))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(postDtoListJson));
+    }
+
+    @Test
+    public void getPostByIdSuccessfully() throws Exception {
+
+        // Arrange
+        var postDto = generator.nextObject(PostDto.class);
+        when(postService.getPostById(anyLong())).thenReturn(postDto);
+        var postDtoJson = mapper.writeValueAsString(postDto);
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(postDtoJson))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(postDtoJson));
+    }
+
+    @Test
+    public void createPostSuccessfully() throws Exception {
+
+        // Arrange
+        PostDto postDto = generator.nextObject(PostDto.class);
+        when(postService.createPost(any(PostDto.class))).thenReturn(postDto);
+        String postDtoJson = new ObjectMapper().writeValueAsString(postDto);
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(postDtoJson))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(postDtoJson));
+    }
+
+    @Test
+    public void updatePostTest() throws Exception {
+
+        // Arrange
+        var originalPostDto = generator.nextObject(PostDto.class);
+        var updatedPostDto = generator.nextObject(PostDto.class);
+        when(postService.updatePost(anyLong(), any(PostDto.class))).thenReturn(updatedPostDto);
+        var postDtoJson = new ObjectMapper().writeValueAsString(originalPostDto);
+
+        // Act & Assert
+        mockMvc.perform(put("/api/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(postDtoJson))
+                        .andExpect(status().isOk())
+                        .andDo(result -> {
+                            PostDto returnedPostDto = new ObjectMapper().readValue(result.getResponse().getContentAsString(), PostDto.class);
+                            assertThat(returnedPostDto).isEqualToComparingFieldByField(updatedPostDto);
+                        });
+    }
+
+    @Test
+    public void deletePostSuccessfully() throws Exception {
+
+        // Arrange
+
+        // Act & Assert
+        mockMvc.perform(delete("/api/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.content().string("Entity deleted successfully"));
+
+        //Verify
+        verify(postService).deletePost(anyLong());
+    }
 }
